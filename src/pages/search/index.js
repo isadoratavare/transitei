acd1b50ea300f3e00c8e4cf35e8a25992b2c4fdd
netoids <75789529+netoids@users.noreply.github.com>
2021-11-25 18:37:10 -0300

    update

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,32 +1,37 @@
 import * as React from 'react';
 import "./index.css";
-
+import grafico from '../../Components/images/grafico.png';
+import icon from "../../Components/images/icon.png"
+import SearchBar from "../../Components/SearchBar"
+import BookData from "../../data/Data.json"
 
 export default function App() {
     return (
       <h2 class="page">
-        <div class="searchbar">
-          <input  placeholder="Pequisar">      
-          </input>
-
+        <div className="App">
+          <SearchBar placeholder="Pesquisar" data={BookData} />
         </div>
-        <div class="security">
-          <div class="security-status">
-            Esta rua é considerada segura
-            <div class="status">
-              Foram registrados x acidentes nesse trajeto entre 2015 e 2021.
-              
-            </div>
+        <section className= "banner">
+          <div class="security">
+            <div class="security-status">Esta rua é considerada segura
+              <div class="status">Foram registrados x acidentes nesse trajeto entre 2015 e 2021.</div>
               <button class="fav-button">
-                Adicionar aos favoritos
+                  <div>
+                    <img src={icon} alt="coração" /> 
+                    <text class="buttom-text"> Adicionar aos favoritos</text>
+                  </div>              
               </button>
-          </div>
-         
-          
+            </div>            
+          </div> 
+          <div2 class= "img-bola">
+            <img src={grafico} alt="grafico" /> 
+          </div2>       
+        </section>
+       
 
-        </div>
+       
         <div class="comments">
-          pt3
+          Comentarios
         </div>        
       </h2>
       
