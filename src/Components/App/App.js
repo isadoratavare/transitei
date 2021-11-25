c5609e1df65b1ccf4b722650e3a9c646bd62f9ca
netoids <75789529+netoids@users.noreply.github.com>
2021-11-25 09:44:05 -0300

    Display_1

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,6 @@ import Search from "../../pages/search";
 
 export default function App() {
   return (
-    <Search />
+      <Search />    
   );
 }
--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,10 +1,34 @@
-import './index.css';
+import * as React from 'react';
+import "./index.css";
+
 
 export default function App() {
     return (
-      <div className ="Page">
-        <button>teste</button>
-      </div>
+      <h2 class="page">
+        <div class="searchbar">
+          <input  placeholder="Pequisar">      
+          </input>
+
+        </div>
+        <div class="security">
+          <div class="security-status">
+            Esta rua é considerada segura
+            <div class="status">
+              Foram registrados x acidentes nesse trajeto entre 2015 e 2021.
+              
+            </div>
+              <button class="fav-button">
+                Adicionar aos favoritos
+              </button>
+          </div>
+         
+          
+
+        </div>
+        <div class="comments">
+          pt3
+        </div>        
+      </h2>
       
     );
   }
