0405f26cd34febd8dbc30deecb96a79edcff2120
netoids <75789529+netoids@users.noreply.github.com>
2021-11-26 18:07:26 -0300

    update

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,37 +1,20 @@
 import * as React from 'react';
 import "./index.css";
-import grafico from '../../Components/images/grafico.png';
-import icon from "../../Components/images/icon.png"
 import SearchBar from "../../Components/SearchBar/SearchBar"
 import BookData from "../../data/Data.json"
+import Body from '../../Components/Body/index'
 
-export default function App() {
-    return (
-      <div class="page">
+export default function Search() {
+  const nome = "Linaldo"
+
+    return (      
+      <div className="page">
         <div className="App">
           <SearchBar placeholder="Pesquisar" data={BookData} />
         </div>
-        <section className= "banner">
-          <div class="security">
-            <div class="security-status">Esta rua é considerada segura
-              <div class="status">Foram registrados x acidentes nesse trajeto entre 2015 e 2021.</div>
-              <button class="fav-button">
-                  <div>
-                    <img src={icon} alt="coração" />
-                    <div2 class="buttom-text"> Adicionar aos favoritos</div2>                     
-                  </div>              
-                  
-              </button>
-            </div>            
-          </div> 
-            <div class= "img-bola">
-              <img src={grafico} alt="grafico" /> 
-            </div>       
-        </section>
-       
+        <Body nome = {nome}/>
 
-       
-        <div class="comments">
+        <div className="comments">
           Comentarios
         </div>        
       </div>
